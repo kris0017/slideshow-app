4d9d136847c0e5f800e9988dc6bc1474bdf4f7c3
Khrystyna Mosolova <kristina.mosolova@gmail.com>
2017-09-26 15:58:38 +0300

    added info btn.

--- src/js/containers/App.js
+++ src/js/containers/App.js
@@ -8,12 +8,15 @@ export default class App extends Component {
     super();
 
     this.state = {
-      countryIndex: 0
+      countryIndex: 0,
+      opacity: 0.25
     };
 
-    this.next = this.next.bind(this);
-    this.previous = this.previous.bind(this);
-    
+    this.next = this.next.bind(this)
+    this.previous = this.previous.bind(this)
+    this.showDescription = this.showDescription.bind(this)
+    this.mouseOver = this.mouseOver.bind(this)
+    this.mouseLeave = this.mouseLeave.bind(this)
   }
 
   previous() {
@@ -28,7 +31,18 @@ export default class App extends Component {
     this.setState({ countryIndex: newIndex }, () => {
       this.canvas.updateCanvas()
     })
-    
+  }
+
+  showDescription() {
+    console.log('fsdfsd')
+  }
+
+  mouseOver() {
+    this.setState({ opacity: 1 })
+  }
+
+  mouseLeave() {
+    this.setState({ opacity: 0.25 })
   }
 
   render() {
@@ -38,20 +52,28 @@ export default class App extends Component {
           <Col md="10">
             <div className="content-container">
               <CanvasComponent ref={instance => { this.canvas = instance; }} />
-              <img className="image" src={ require('../../img/' + db.countries[this.state.countryIndex].image) } alt={ db.countries[this.state.countryIndex].image }/>
+              <img src={ require('../../img/' + db.countries[this.state.countryIndex].image) } 
+                   alt={ db.countries[this.state.countryIndex].image }
+                   className="image rounded" 
+                   onMouseOver={ this.mouseOver } 
+                   onMouseLeave={ this.mouseLeave } />
+              <Button style={{opacity: this.state.opacity}} 
+                      className="btn-show-info" size="lg" 
+                      onMouseOver={ this.mouseOver }  
+                      onClick={ this.showDescription }></Button>
             </div>
           </Col>
           
         </Row>
-        <Row className="justify-content-center btn-title-container">
+        <Row className="justify-content-center">
           <Col md="1" className="col-auto">
-            <Button className="prev-btn" color="info" size="lg" onClick={ this.previous }>&lt;</Button>
+            <Button color="info" size="lg" onClick={ this.previous }>&lt;</Button>
           </Col>
           <Col md="8" className="col-auto">
-            <h6 className="vertical-center">{ db.countries[this.state.countryIndex].name }</h6>
+            <h6 className="vertical-center text-center font-weight-bold">{ db.countries[this.state.countryIndex].name }</h6>
           </Col>
           <Col md="1" className="col-auto">
-            <Button className="next-btn" color="info" size="lg" onClick={ this.next }>&gt;</Button>
+            <Button className="float-right" color="info" size="lg" onClick={ this.next }>&gt;</Button>
           </Col>
         </Row>
       </Container>
